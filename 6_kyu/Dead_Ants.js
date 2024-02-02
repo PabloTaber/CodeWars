afe1dbfcfb1f470a7cea9f3d8ed68afdb1944298
PabloTaber <pablo.tabernerovidal@gmail.com>
2024-02-02 21:21:40 +0100

    Actualizacion

--- 6_kyu/Dead_Ants.js
+++ 6_kyu/Dead_Ants.js
@@ -1,19 +1,23 @@
-const deadAntCount = ants => {
-    let countA = 0;
-    let countN = 0;
-    let countT = 0;
-
-    const sinAnts = ants.replace(/ant/gi, '') || '';
-
-    for (let i = 0; i < sinAnts.length; i++) {
-        if (sinAnts[i] === 'a') {
-            countA++;
-        } else if (sinAnts[i] === 'n') {
-            countN++;
-        } else if (sinAnts[i] === 't') {
-            countT++;
-        }
-    }
-
-    return Math.max(countA, countN, countT);
+const deadAntCount = (ants) => {
+  if (ants == null) {
+    return 0;
   }
+
+  let countA = 0;
+  let countN = 0;
+  let countT = 0;
+
+  const sinAnts = ants.replace(/ant/gi, "") || "";
+
+  for (let i = 0; i < sinAnts.length; i++) {
+    if (sinAnts[i] === "a") {
+      countA++;
+    } else if (sinAnts[i] === "n") {
+      countN++;
+    } else if (sinAnts[i] === "t") {
+      countT++;
+    }
+  }
+
+  return Math.max(countA, countN, countT);
+};
